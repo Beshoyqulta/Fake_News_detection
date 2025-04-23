f59902b60a4cfdad0b542c69610a588614d18298
Mohamed Zabady <zabadymohamed8@gmail.com>
2025-04-23 21:36:45 +0200

    chore: updating the app to has only the fastapi app

--- app.py
+++ app.py
@@ -1,21 +1,14 @@
-from duckduckgo_search import DDGS 
-from google.generativeai import generative_models
-from sklearn.metrics.pairwise import cosine_similarity
-from langchain_huggingface import HuggingFaceEmbeddings , HuggingFaceEndpoint
-from typing import *
-search = DDGS()
-LLM = generative_models.GenerativeAIModel("gemini-1.5-flash")
-
-class NewsRequest:
-    input_str: str
-def embed(text: str):
-    return HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2").embed_query(text)
-def search(query: str):
-    results = search.news(query)
-    return results
-def check_relevance(query: str, result: str):
-    cosine_similarity = cosine_similarity([embed(query)], [embed(result)])
-    if cosine_similarity > 0.6 and cosine_similarity < 1:
-        return True
-    else:
-        return False
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import os
+from functions import generate_content, NewsRequest
+app = FastAPI()
+@app.post("/Trustness/")  # This line decorates 'translate' as a POST endpoint
+async def translate(request: NewsRequest):
+    try:
+        # Call your translation function
+        trusted_text = generate_content(request.input_str)
+        return {"trusted_text": trusted_text}
+    except Exception as e:
+        # Handle exceptions or errors during translation
+        raise HTTPException(status_code=500, detail=str(e))
